2f6136be17e427a95d4fffb1ffb772da36e797fc
buihien0109 <buihien01091997@gmail.com>
2024-05-28 19:18:19 +0700

    update

--- springboot/day_10/movie-app/src/main/java/com/example/movieapp/controller/MovieController.java
+++ springboot/day_10/movie-app/src/main/java/com/example/movieapp/controller/MovieController.java
@@ -5,10 +5,12 @@ import com.example.movieapp.repository.ActorRepository;
 import com.example.movieapp.repository.CountryRepository;
 import com.example.movieapp.repository.DirectorRepository;
 import com.example.movieapp.repository.GenreRepository;
+import com.example.movieapp.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,17 +21,27 @@ public class MovieController {
     private final DirectorRepository directorRepository;
     private final ActorRepository actorRepository;
     private final GenreRepository genreRepository;
+    private final MovieService movieService;
 
     // Tạo movie (template)
     @GetMapping("/create")
     public String getCreatePage(Model model) {
-        // Trả về ds quốc gia, đạo diễn, diễn viên, thể loại, loại phim
-        // TODO: Refactor theo controller - service - repo
         model.addAttribute("countries", countryRepository.findAll());
         model.addAttribute("directors", directorRepository.findAll());
         model.addAttribute("actors", actorRepository.findAll());
         model.addAttribute("genres", genreRepository.findAll());
         model.addAttribute("movieTypes", MovieType.values());
         return "admin/movie/create";
+    }
+
+    @GetMapping("/{id}")
+    public String getDetailPage(Model model, @PathVariable Integer id) {
+        model.addAttribute("movie", movieService.getMovieById(id));
+        model.addAttribute("countries", countryRepository.findAll());
+        model.addAttribute("directors", directorRepository.findAll());
+        model.addAttribute("actors", actorRepository.findAll());
+        model.addAttribute("genres", genreRepository.findAll());
+        model.addAttribute("movieTypes", MovieType.values());
+        return "admin/movie/detail";
     }
 }
